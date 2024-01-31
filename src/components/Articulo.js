acebc3cad537bd00be0e16c443c7ae67a2374f32
killerzero337 <metalmaster37yt@gmail.com>
2024-01-31 19:42:39 +0100

    ....

--- src/components/Articulo.js
+++ src/components/Articulo.js
@@ -3,9 +3,9 @@
 function Articulo({ children, articulo }) {
     return (
         <div style={{ 'border': '1px solid lightgrey', 'padding': '50px' }}>
-            <p><strong>{articulo.nombre}</strong></p>
-            <p>{articulo.descripcion}</p>
-            <p>{articulo.precio.toString()} €</p>
+            <p><strong>{articulo?.nombre}</strong></p>
+            <p>{articulo?.descripcion}</p>
+            <p>{articulo?.precio?.toString()} €</p>
             {children}
         </div>
     )
